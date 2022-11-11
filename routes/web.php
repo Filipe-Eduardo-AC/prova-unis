aa544b82d6f65cac2181792c030ce60a57edb094
Filipe Eduardo Andrade Costa <filipeeacosta@gmail.com>
2022-11-11 11:32:23 -0300

    API Laravel (incompleta)

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Http;
+use App\Http\Controllers\apiController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', function () {
+    $api = Http::get('https://servicodados.ibge.gov.br/api/v1/projecoes/populacao/{localidade}');
+    $apiArray = $api->json();
+
+    return view('projecao', ['apiArray' => $apiArray]);
+});
